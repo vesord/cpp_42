5c68f3cc1bc240a94067b87965b98c4782fe9a09
Matrus <matrus@student.21-school.ru>
2020-11-03 04:31:13 +0300

    m8e2 refactor

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -23,9 +23,7 @@ class MutantStack : public std::stack< T > {
 public:
 
 	MutantStack() : std::stack<T>() { };
-
 	virtual ~MutantStack() { };
-
 	MutantStack(MutantStack const & mutantstack) : std::stack<T>( mutantstack ) { };
 
 	MutantStack & operator=(MutantStack const & mutantstack) {
@@ -36,23 +34,36 @@ public:
 class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
 
 public:
-	iterator();
-	~iterator();
+	iterator() { this->_ptr = nullptr; };
+	~iterator() { };
 
-	MutantStack<T>::iterator & operator=( MutantStack<T>::iterator const & );
-	iterator( MutantStack<T>::iterator const & );
-	iterator( T * );
+	MutantStack<T>::iterator & operator=( MutantStack<T>::iterator const & rhs ) {
+		if (this == &rhs)
+			return *this;
+		this->_ptr = rhs._ptr;
+		return *this;
+	}
+	iterator( MutantStack<T>::iterator const & iterator1 ) { *this = iterator1; };
+	iterator( T * ptr ) { this->_ptr = ptr; };
 
-	bool operator==( MutantStack<T>::iterator const & );
-	bool operator!=( MutantStack<T>::iterator const & );
+	bool operator==( MutantStack<T>::iterator const & rhs ) { return this->_ptr == rhs._ptr; };
+	bool operator!=( MutantStack<T>::iterator const & rhs ) { return this->_ptr != rhs._ptr; };
 
-	MutantStack<T>::iterator & operator++();
-	MutantStack<T>::iterator operator++(int);
+	MutantStack<T>::iterator & operator++() { this->_ptr -= 1; return *this; };
+	MutantStack<T>::iterator operator++(int) {
+		MutantStack<T>::iterator tmp = *this;
+		this->operator++();
+		return tmp;
+	};
 
-	MutantStack<T>::iterator & operator--();
-	MutantStack<T>::iterator operator--(int);
+	MutantStack<T>::iterator & operator--() { this->_ptr += 1; return *this; };
+	MutantStack<T>::iterator operator--(int) {
+		MutantStack<T>::iterator tmp = *this;
+		this->operator--();
+		return tmp;
+	};
 
-	T & operator*();
+	T & operator*() { return *(this->_ptr); };
 
 private:
 
@@ -60,93 +71,11 @@ private:
 
 };
 
-	MutantStack<T>::iterator begin();
-	MutantStack<T>::iterator end();
+	MutantStack<T>::iterator begin() { return iterator(&this->top()); };
+	MutantStack<T>::iterator end() { return iterator(&this->top() - this->size()); };
 
 private:
 
 };
-
-template < typename T >
-typename MutantStack<T>::iterator MutantStack<T>::end()
-{
-	return iterator(&this->top() - this->size());
-}
-
-template < typename T >
-typename MutantStack<T>::iterator MutantStack<T>::begin()
-{
-	return iterator(&this->top());
-}
-
-template < typename T >
-MutantStack<T>::iterator::iterator() {
-	this->_ptr = nullptr;
-}
-
-template < typename T >
-MutantStack<T>::iterator::~iterator() { }
-
-template < typename T >
-MutantStack<T>::iterator::iterator(const MutantStack<T>::iterator & iterator1) {
-	*this = iterator1;
-}
-
-template < typename T >
-typename MutantStack<T>::iterator & MutantStack<T>::iterator::operator=(
-	const MutantStack<T>::iterator & rhs)
-{
-	if (this == &rhs)
-		return *this;
-
-	this->_ptr = rhs._ptr;
-	return *this;
-}
-
-template < typename T >
-MutantStack<T>::iterator::iterator(T * ptr) {
-	this->_ptr = ptr;
-}
-
-template < typename T >
-bool MutantStack<T>::iterator::operator==(const MutantStack<T>::iterator & rhs) {
-	return this->_ptr == rhs._ptr;
-}
-
-template < typename T >
-bool MutantStack<T>::iterator::operator!=(const MutantStack<T>::iterator & rhs) {
-	return this->_ptr != rhs._ptr;
-}
-
-template < typename T >
-typename MutantStack<T>::iterator & MutantStack<T>::iterator::operator++() {
-	this->_ptr -= 1;
-	return *this;
-}
-
-template < typename T >
-typename MutantStack<T>::iterator MutantStack<T>::iterator::operator++(int) {
-	MutantStack<T>::iterator tmp = *this;
-	this->operator++();
-	return tmp;
-}
-
-template < typename T >
-typename MutantStack<T>::iterator & MutantStack<T>::iterator::operator--() {
-	this->_ptr += 1;
-	return *this;
-}
-
-template < typename T >
-typename MutantStack<T>::iterator MutantStack<T>::iterator::operator--(int) {
-	MutantStack<T>::iterator tmp = *this;
-	this->operator--();
-	return tmp;
-}
-
-template < typename T >
-T & MutantStack<T>::iterator::operator*() {
-	return *(this->_ptr);
-}
 
 #endif
